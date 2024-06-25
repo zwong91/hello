af175751780b73a39ef1e99934c971ec5f9ed62c
jong-un <36259838+zwong91@users.noreply.github.com>
2024-06-25 19:56:42 +0800

    Update cards.js

--- src/components/cards.js
+++ src/components/cards.js
@@ -28,9 +28,9 @@ export const Cards = () => {
         <p>Discover how simple it is to interact with a Utility smart contract.</p>
 
         <h2>
-          Utility Net Faucet, Replace receiverId funds account <span>-&gt;</span>
+          Utility Net Faucet, Replace receiverId funds account, such httpie cli <span>-&gt;</span>
         </h2>
-        <p>curl -X POST -H "Content-Type: application/json" -d '{{"amount":"10000000000000000000000000", "receiverId":"67c7616cb8472180c19975f04e9b320d6f07d197d65b137dcffc40e7d25df6de", "contractId":"4e0375672ec30f2efe3a6c5a14ff81d37f1271c439501eac2fb445df262b2c32"}}' https://unc-faucet.xyz666.org/api/faucet/tokens </p>
+        <pre><code>http POST https://unc-faucet.xyz666.org/api/faucet/tokens Content-Type:application/json amount=10000000000000000000000000 receiverId=67c7616cb8472180c19975f04e9b320d6f07d197d65b137dcffc40e7d25df6de contractId=4e0375672ec30f2efe3a6c5a14ff81d37f1271c439501eac2fb445df262b2c32</code></pre>
       </Link>
     </div>
   );
